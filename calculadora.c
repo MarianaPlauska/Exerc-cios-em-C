b3d13ec759f52872cd71c6f48e749489d134de1a
Mariana Plauska <103209930+MarianaPlauska@users.noreply.github.com>
2022-11-08 21:16:52 -0300

    Calculadora
    Calculadora simples feita em C.

---
+++ calculadora.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int operacoes (float a, float b, int op) 
+{
+    int resultado;
+
+    switch (op) 
+    {
+        case 1:
+            resultado = a + b;
+            break;
+        case 2:
+            resultado = a - b;
+            break;
+        case 3:
+            resultado = a * b;
+            break;
+        case 4:
+            resultado = a / b;
+            break;
+        default:
+            printf("Operacao invalida \n");
+            break;
+    }
+    return resultado;
+}
+
+void main ()
+{
+    int op;
+    float a, b, resultado;
+    
+    printf ("Digite o primeiro numero: \n");
+    scanf ("%f", &a);
+
+    printf ("Digite o segundo numero: \n");
+    scanf ("%f", &b);
+
+    printf ("\t\t Menu de opcoes: \n");
+    printf ("\t \t 1 - Soma \n");
+    printf ("\t \t 2 - Subtracao \n");
+    printf ("\t \t 3 - Multiplicacao \n");  
+    printf ("\t \t 4 - Divisao \n");
+    printf ("\t \t 5 - Sair \n");
+
+    printf ("\nDigite a opcao desejada: ");
+    scanf ("%d", &op);
+
+    resultado = operacoes (a, b, op);
+
+    printf ("%1.f \n", resultado);
+}
